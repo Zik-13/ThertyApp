5d779076166aa559917fa7292e458185c19b0aac
zikcruel@mail.ru <zikcruel@mail.ru>
2022-11-19 18:12:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/thertyapp/LoginMenu.java
@@ -0,0 +1,17 @@
+package com.example.thertyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class LoginMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
